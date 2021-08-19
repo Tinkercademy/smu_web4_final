28c944810a6d207256d9171ad462f01bdd1f923b
YJ Soon <yjsoon@tinkertanker.com>
2021-08-19 15:31:18 +0800

    Updated db config

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,4 +1,22 @@
 <?php
-$con = mysqli_connect("localhost:3308", "root", "") or die("Unable to connect");
-mysqli_select_db($con, "movie_reviews");
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = ''; // use root for MAMP
+$db_db = 'movie_reviews'; // check against your DB name
+
+$mysqli = @new mysqli(
+    $db_host,
+    $db_user,
+    $db_password,
+    $db_db
+);
+
+if ($mysqli->connect_error) {
+    echo 'Errno: ' . $mysqli->connect_errno;
+    echo '<br>';
+    echo 'Error: ' . $mysqli->connect_error;
+    exit();
+}
+
+$con = $mysqli; // we use $con in our code
 ?>
